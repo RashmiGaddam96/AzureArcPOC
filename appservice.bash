4c3b7ea69ac3a7c8b2ffd6be190e0c9f1de28a18
RashmiGaddam96 <89255884+RashmiGaddam96@users.noreply.github.com>
2021-09-09 11:25:14 +0530

    Add files via upload

---
+++ appservice.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export location=$LOCATION
+export resourceGroup=$RESOURCEGROUP
+export clusterName=$CLUSTERNAME
+export arcClusterName="${resourceGroup}-arcCluster"
+export workspaceName=$WORKSPACENAME
+export groupName=$resourceGroup
+export customLocationName=$CUSTOMLOCATIONNAME
+export appresourceGroup="${resourceGroup}-AppService"
+export webAppName=$WEBAPPNAME
+export myPlanName=$MYPLANNAME
+
+
+az config set extension.use_dynamic_install=yes_without_prompt
+az extension add --upgrade --yes --name customlocation
+az extension remove --name appservice-kube
+az extension add --yes --source "https://aka.ms/appsvc/appservice_kube-latest-py2.py3-none-any.whl"
+
+customLocationId=$(az customlocation show \
+    --resource-group $groupName \
+    --name $customLocationName \
+    --query id \
+    --output tsv)
+
+echo "Creating Azure App Resource Group"
+if [ $(az group exists --name $appresourceGroup) = false ]; then
+    az group create --name $appresourceGroup --location $location
+fi
+az appservice plan create -g $appresourceGroup -n $myPlanName \
+    --custom-location $customLocationId \
+    --per-site-scaling --is-linux --sku K1
+
+az webapp create \
+    --plan $myPlanName \
+    --resource-group $appresourceGroup \
+    --name $webAppName \
+    --custom-location $customLocationId \
+    --runtime 'DOTNET|5.0'
+
+# az webapp deployment source config-zip --resource-group $appresourceGroup --name $webAppName --src package.zip
+
